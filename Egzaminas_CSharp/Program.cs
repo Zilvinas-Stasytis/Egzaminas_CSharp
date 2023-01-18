edb21fa3adb989947c59dbe877d959b5b46f36d6
Žilvinas Stasytis <zalciukas@gmail.com>
2023-01-18 20:33:44 +0200

    Add project files.

---
+++ Egzaminas_CSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant_CSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Staliukai smallTable = new Staliukai("","",false);
+          
+            smallTable.StaliukuSarasas();
+        }
+    }
+}
---
+++ Egzaminas_CSharp/Staliukai.cs
@@ -0,0 +1,51 @@
+﻿namespace Restaurant_CSharp
+{
+    public class Staliukai
+    {
+        string ID;
+        string vietuSkaicius;
+        bool uzimtumas;
+
+        public Staliukai(string _ID, string _vietuSkaicius, bool _uzimtumas)
+        {
+            Console.WriteLine("Įveskite staliuko ID");
+           _ID = Console.ReadLine();
+            Console.WriteLine("Įveskite staliuko vietų skaičių");
+            _vietuSkaicius = Console.ReadLine();
+
+            ID = _ID;
+            vietuSkaicius = _vietuSkaicius;
+            uzimtumas = _uzimtumas;
+        }
+        public string Id
+        {
+            get { return ID; }
+            set { ID = value; }
+        }
+        public string VietuSkaicius
+        {
+            get { return vietuSkaicius; }
+            set { vietuSkaicius = value; }
+        }
+        public bool Uzimtumas
+        {
+            get { return uzimtumas; }
+            set { uzimtumas = value; }
+        }
+    
+        public void StaliukuSarasas()
+        {
+            List<string> staliukuSarasas = new List<string>();
+            /*Console.WriteLine("Įveskite staliuko užimtumą");
+           Console.ReadLine(Convert.ToBoolean(uzimtumas));*/
+            string[] tables = {
+                ID,
+                vietuSkaicius
+            };
+            staliukuSarasas.AddRange(tables);
+            Console.WriteLine($"{ID}");
+            int ssk = staliukuSarasas.Count;
+            Console.WriteLine($"Staliukų yra {ssk}");
+        }
+    }
+}
